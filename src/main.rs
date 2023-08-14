c95c595778dd7b6ddea80f1e14d43cc908a34b6b
lthoerner <lowellashton@gmail.com>
2023-08-13 21:31:08 -0700

    add HTML file with video

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::body::Bytes;
+use axum::response::Html;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(foo))
+        .route("/sample_video.mp4", get(serve_video));
+
+    let socket = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
+    axum::Server::bind(&socket)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn foo() -> Html<&'static str> {
+    Html(include_str!("../assets/foo.html"))
+}
+
+async fn serve_video() -> Bytes {
+    Bytes::from_static(include_bytes!("../assets/sample_video.mp4"))
 }
